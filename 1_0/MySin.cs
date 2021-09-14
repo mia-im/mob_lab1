a771348fceef2bd79b69028808f2ed7a30f8c7dc
mia-im <62039441+mia-im@users.noreply.github.com>
2021-09-15 01:20:02 +0300

    Add files via upload

---
+++ 1_0/MySin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MyLib
+{
+    /// Calculation of mathematical functions
+    public class MySin
+    {
+        /// <summary>
+        /// Sin(x)
+        /// </summary>
+        /// <param name="x">the angle in radians - the first argument of the Sin function</param>
+        /// <param name="n">the exponent - the second argument of the Sin function</param>
+        /// <returns>Returns the value of the Sin function for a given angle</returns>
+        public static double Sin(double x, int n)
+        {
+            double result = 0;
+            for (int i=0; i<n; i++)
+            {
+                result = result + (Math.Pow((-1), i) * Math.Pow(x, (2 * i + 1))) / F(2 * i + 1);
+            }
+            return result;
+        }
+        static double F(int n)
+        {
+            double tmp = 1;
+            for(int i=1; i<=n; i++)
+            {
+                tmp = tmp * i;
+            }
+            return tmp;
+        }
+
+    }
+}
---
+++ 1_0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleMySin
+{
+    class Program
+    {
+        /// <summary>
+        /// Calling the library method Sin (x, n) from MySin.dll
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите х - угол в радианах");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите показатель степени n");
+            int n = int.Parse(Console.ReadLine());
+            // call the sin (x) calculation method from the library
+            double my_sinus = MyLib.MySin.Sin(x, n);
+            // call a method from the class Math
+            double sinus = Math.Sin(x);
+            double delta = sinus - my_sinus;
+            Console.WriteLine("my_sinus={0}, sin={1}, delta={2}", my_sinus, sinus, delta);
+            Console.ReadKey();
+        }
+    }
+}
